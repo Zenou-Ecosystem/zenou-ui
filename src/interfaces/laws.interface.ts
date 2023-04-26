214e08653a613673a16e9e3ae13b9b457c31e15d
Nsama david <nsamadavidazaah@gmail.com>
2023-04-27 00:40:17 +0100

    add law v1: revamp functioning

--- src/interfaces/laws.interface.ts
+++ src/interfaces/laws.interface.ts
@@ -1,24 +1,32 @@
+
+type ILawOptionTitles = "decree" | "order" | "decision";
+type ILawTitles = ['convention', 'law', 'decree', 'order', 'decisions', 'notes', 'guidance', 'direction'];
+type ILawLocations = ['international', 'continental', 'national'];
+type ILawCompliance = ['complaint', 'non-compliant', 'in progress'];
+type ILawDomains = ["air", "land", "water", "environment", "business", "education", "transport", "health", "agriculture"];
+type ILawDecisions = ['informative', 'administrative', 'financial'];
+
 export interface ILaws {
-    title: string;
-    location: string;
+    title: keyof ILawTitles;
+    location: keyof ILawLocations;
     ratification: string;
     area: string;
     theme: string;
     link: string;
-    area_number: string;
+    item_number: string;
     paragraph_number: string;
-    decisions: string;
-    compliance: string;
+    decisions: keyof ILawDecisions;
+    compliance: keyof ILawCompliance;
+    domain: keyof ILawDomains;
+    articles: string;
+    severity: number;
     control_plan: string;
     action_plan: string;
-    options?: Record<decisionTitles, Decision[]>;
+    options?: Record<ILawOptionTitles, ILawOptions[]>;
 }
 
-type decisionTitles = "decree" | "order" | "decision"
-
-interface Decision {
+interface ILawOptions {
     id: string;
     title: string;
-    reference: string;
-    description: string | null;
+    dependsOn: string // id of the selected choice
 }
